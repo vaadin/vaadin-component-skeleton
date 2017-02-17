3dce17e17f0b2a84fb1de381ed7cab56bcc15cb8
Limon Monte <limon.monte@gmail.com>
2017-02-17 12:54:37 +0200

    Visual testing: replace gemini-express by polyserve

--- test/visual/test.js
+++ test/visual/test.js
@@ -2,13 +2,15 @@ gemini.suite('vaadin-element', function(rootSuite) {
 
   rootSuite.setUrl('/');
 
-  gemini.suite('button', function(suite) {
+  gemini.suite('vaadin-element', function(suite) {
     suite
       .setCaptureElements('#visual-tests')
       .before(function(actions, find) {
         this.button = find('button');
       })
-      .capture('normal-button')
+      .capture('normal-button', {}, function(actions, find) {
+        actions.wait(3000); // hack for IE/Edge
+      })
       .capture('clicked-button', function(actions) {
         actions.mouseDown(this.button);
       });
