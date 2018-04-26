3ffc8616c4b2ab42aedde4227748103b8bfb9949
web-padawan <serguey.kulikov@gmail.com>
2018-04-26 11:17:49 +0300

    Run P3 tests in all browsers. Bump node to 8.11

--- wct.conf.js
+++ wct.conf.js
@@ -2,37 +2,53 @@ var envIndex = process.argv.indexOf('--env') + 1;
 var env = envIndex ? process.argv[envIndex] : undefined;
 
 module.exports = {
+  testTimeout: 180 * 1000,
+  verbose: true,
   registerHooks: function(context) {
-    var saucelabsPlatforms = [
-      'Windows 10/chrome@65',
-      'Windows 10/firefox@59',
+    const saucelabsPlatformsMobile = [
       'macOS 10.12/iphone@10.3',
-      'macOS 10.12/ipad@11.0',
-      'Windows 10/microsoftedge@15',
-      'Windows 10/internet explorer@11',
-      'macOS 10.12/safari@11.0',
+      'macOS 10.12/ipad@11.2',
       'macOS 9.3.2/iphone@9.3'
     ];
 
-    var saucelabsPlatformsP3 = [
-      'macOS 10.12/iphone@11.2',
-      'macOS 10.12/ipad@11.2',
+    const saucelabsPlatformsPolyfilled = [
+      'Windows 10/microsoftedge@16',
+      'Windows 10/microsoftedge@15',
+      'Windows 10/internet explorer@11'
+    ];
+
+    const saucelabsPlatformsDesktop = [
       'Windows 10/chrome@65',
+      'Windows 10/firefox@59',
       'macOS 10.12/safari@11.0'
     ];
 
-    var cronPlatforms = [
+    const cronPlatforms = [
       'Android/chrome',
       'Windows 10/chrome@65',
       'Windows 10/firefox@59'
     ];
 
-    if (env === 'saucelabs') {
-      context.options.plugins.sauce.browsers = saucelabsPlatforms;
-    } else if (env === 'saucelabs-p3') {
-      context.options.plugins.sauce.browsers = saucelabsPlatformsP3;
-    } else if (env === 'saucelabs-cron') {
-      context.options.plugins.sauce.browsers = cronPlatforms;
+    switch (env) {
+      case 'saucelabs:mobile':
+        context.options.plugins.sauce.browsers = saucelabsPlatformsMobile;
+        break;
+      case 'saucelabs:polyfilled':
+        context.options.plugins.sauce.browsers = saucelabsPlatformsPolyfilled;
+        break;
+      case 'saucelabs:desktop':
+        context.options.plugins.sauce.browsers = saucelabsPlatformsDesktop;
+        break;
+      case 'saucelabs-cron':
+        context.options.plugins.sauce.browsers = cronPlatforms;
+        break;
+      case 'saucelabs':
+        context.options.plugins.sauce.browsers = cronPlatforms.concat(
+          saucelabsPlatformsDesktop,
+          saucelabsPlatformsMobile,
+          saucelabsPlatformsPolyfilled
+        );
+        break;
     }
   }
 };
